2922b475e925e5f7a93ac4b27f43654f19b04fa3
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-14 23:04:54 +0200

    partially done with enter raffle function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,21 +7,32 @@ pragma solidity ^0.8.19;
  * @notice This contract is for creating a simple raffle
  * @dev Implements Chainlink VRFv---
  */
-
 contract Raffle {
+    /* Errors */
+    error Raffle__SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players
+
+    /**events */
+    event RaffleEntered(address indexed player);
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee; //will be in native token
     }
 
-    function enterRagffle () public payable {}
+    function enterRagffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        s_players.push(payable(msg.sender))
+        emit RaffleEntered(msg.sender);
 
-    function pickWinner () public {}
-
-    function getEntranceFee () external view returns(uint256) {
-        return i_entranceFee;
     }
 
+    function pickWinner() public {}
+
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
 }
