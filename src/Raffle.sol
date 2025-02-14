0881d7d3534c41a93fd89239b15de8f9f9d477c5
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-14 20:57:34 +0200

    initialised raffle contract:

---
+++ src/Raffle.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**
+ * @title A sample Raffle contract
+ * @author Syber88
+ * @notice This contract is for creating a simple raffle
+ * @dev Implements Chainlink VRFv---
+ */
+
+contract Raffle {
+
+    function enterRagffle () public {}
+
+    function pickWinner () public {}
+
+}
