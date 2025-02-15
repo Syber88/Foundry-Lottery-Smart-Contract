3860ebc75f23e6a6d4c93a112ef933d6f1607e33
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2025-02-15 23:11:40 +0200

    initialised helper config to deal with scripting and deploying of the contract

---
+++ script/DeployRaffle.s.sol
@@ -0,0 +1,15 @@
+// SPDX-Licende-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+contract DeployRaffle is Script {
+    function run(){
+
+    }
+
+    function deployContract() public returns(Raffle, HelperConfig ) {
+
+    }
+
+}
